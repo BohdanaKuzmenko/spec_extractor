67e5990c75cc267b68723283176c188c323a858a
bogdana_kuzmenko <bohdana.kuzmenko@introlab-systems.com>
2016-05-18 16:04:44 +0300

    kill_proc_added

--- app/services/check_bios/main.py
+++ app/services/check_bios/main.py
@@ -5,6 +5,7 @@ import multiprocessing
 from pandas import concat, Series, set_option
 from app.services.check_bios.text_normalizer import sentences_splitter
 import numpy as np
+import os, signal, sys, subprocess
 
 
 class Extractor(object):
@@ -15,15 +16,24 @@ class Extractor(object):
         set_option('display.max_colwidth', -1)
         p = multiprocessing.Pool(4)
         pool_results = p.map(self.filter_with_regex, np.array_split(bios, 4))
+        signal.signal(signal.SIGTERM, self.kill_child_processes)
         p.close()
         p.join()
         concatenated = concat(pool_results)
         urls = list(set(concatenated["profileUrl"].values.tolist()))
         result = bios[bios["profileUrl"].isin(urls)]
-        result['profileUrl'] = result['profileUrl'].apply(lambda x: '<a href="{}">{}</a>'.format(x,x))
+        result['profileUrl'] = result['profileUrl'].apply(lambda x: '<a href="{}">{}</a>'.format(x, x))
         result['attorneyBio'] = result['attorneyBio'].apply(lambda x: '<p class = "test">{}</p>'.format(x))
         return result
 
+    def kill_child_processes(self, signum, frame):
+        parent_id = os.getpid()
+        ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % parent_id, shell=True, stdout=subprocess.PIPE)
+        ps_output = ps_command.stdout.read()
+        retcode = ps_command.wait()
+        for pid_str in ps_output.strip().split("\n")[:-1]:
+            os.kill(int(pid_str), signal.SIGTERM)
+        sys.exit()
 
     def filter_with_regex(self, bio_df):
         splitted_bios = concat([Series(row['profileUrl'], sentences_splitter(row['attorneyBio']))
